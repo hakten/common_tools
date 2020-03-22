26895e355f9f766c1739e7d51260d5d31467702d
Huseyin Akten <husakten@gmail.com>
2020-03-22 17:10:50 -0500

    names are normalized

--- google_k8s_service_nexus_module.tf
+++ google_k8s_service_nexus_module.tf
@@ -2,13 +2,13 @@ module "nexus_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "nexusnew"
   deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "nexusne.${var.google_domain_name}"
+  deployment_endpoint    = "nexus.${var.google_domain_name}"
   deployment_path        = "nexus"
 
   
   template_custom_vars = {
     
-    docker_endpoint          = "dockerne.${var.google_domain_name}"
+    docker_endpoint          = "docker.${var.google_domain_name}"
     nexus_port               = "${var.nexus_service_port}"
     repo_port                = "${var.repo_port}"
   }
