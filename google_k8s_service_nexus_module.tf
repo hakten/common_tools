aac2d1aad99ac7ebd9a56ceac065954e382b249e
Huseyin Akten <husakten@gmail.com>
2020-03-22 16:00:29 -0500

    ingress docker added

--- google_k8s_service_nexus_module.tf
+++ google_k8s_service_nexus_module.tf
@@ -5,8 +5,8 @@ module "nexus_deploy" {
   deployment_endpoint    = "nexusnew.${var.google_domain_name}"
   deployment_path        = "nexus"
 
+  
   template_custom_vars = {
-    
     
     docker_endpoint          = "dockernew.${var.google_domain_name}"
     nexus_port               = "${var.nexus_service_port}"
