2a43cfe365e51f811c2ecbee029e1cbcb41348aa
Huseyin Akten <husakten@gmail.com>
2020-03-22 16:15:01 -0500

    module adjusted for helm chart

--- google_k8s_service_nexus_module.tf
+++ google_k8s_service_nexus_module.tf
@@ -1,14 +1,14 @@
 module "nexus_deploy" {
   source  = "fuchicorp/chart/helm"
-  deployment_name        = "nexus-new"
+  deployment_name        = "nexusnew"
   deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "nexusnew.${var.google_domain_name}"
+  deployment_endpoint    = "nexusne.${var.google_domain_name}"
   deployment_path        = "nexus"
 
   
   template_custom_vars = {
     
-    docker_endpoint          = "dockernew.${var.google_domain_name}"
+    docker_endpoint          = "dockerne.${var.google_domain_name}"
     nexus_port               = "${var.nexus_service_port}"
     repo_port                = "${var.repo_port}"
   }
