76cc4127d0225daa4c6f0478f083a45a06b2cd5b
Huseyin Akten <husakten@gmail.com>
2020-03-27 02:43:45 -0500

    depends on removed

--- google_k8s_dashboard_module.tf
+++ google_k8s_dashboard_module.tf
@@ -1,6 +1,5 @@
 module "dashboard_deployer" {
-  module_depends_on = ["null_resource.helm_init"]
-  source  = "fuchicorp/chart/helm"
+
   deployment_name        = "dashboard-${var.deployment_environment}"
   deployment_environment = "kube-system"
   deployment_endpoint    = "dashboard.${var.google_domain_name}"
