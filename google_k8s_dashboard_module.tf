6749f0c99b2005e8dc37331455239b79ad07506d
Huseyin Akten <husakten@gmail.com>
2020-03-27 02:44:36 -0500

    source added for dashboard

--- google_k8s_dashboard_module.tf
+++ google_k8s_dashboard_module.tf
@@ -1,5 +1,5 @@
 module "dashboard_deployer" {
-
+  source  = "fuchicorp/chart/helm"
   deployment_name        = "dashboard-${var.deployment_environment}"
   deployment_environment = "kube-system"
   deployment_endpoint    = "dashboard.${var.google_domain_name}"
