410a6529eea1d4163b37b2550fbe2edd4e7b192a
Huseyin Akten <husakten@gmail.com>
2020-03-27 11:15:08 -0500

    interpolation added for dependsing

--- google_k8s_service_grafana-module.tf
+++ google_k8s_service_grafana-module.tf
@@ -6,17 +6,17 @@ module "grafana_deploy" {
   deployment_path        = "grafana"
 
   template_custom_vars = {
-    
+
+    deneme                   = "${null_resource.helm_init}"
     
     datasource_dns_endpoint  = "https://prometheus.${var.google_domain_name}"
     grafana_password         = "${var.grafana["grafana_password"]}"
     grafana_username         = "${var.grafana["grafana_username"]}"
     grafana_client_secret    = "${var.grafana["grafana_client_secret"]}"
     grafana_auth_client_id   = "${var.grafana["grafana_auth_client_id"]}"
-
+    
     smtp_user                = "${var.grafana["smtp_username"]}"
     smtp_password            = "${var.grafana["smtp_password"]}"
     smtp_host                = "${var.grafana["smtp_host"]}"
-    
   }
 }
