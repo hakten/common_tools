e28bc6ecd6133eac1c791c8ffbaad80788869a49
Huseyin Akten <husakten@gmail.com>
2020-03-27 02:22:00 -0500

    module dpends on being tried

--- google_k8s_dashboard_module.tf
+++ google_k8s_dashboard_module.tf
@@ -1,4 +1,5 @@
 module "dashboard_deployer" {
+  depends_on = ["null_resource.helm_init"]
   source  = "fuchicorp/chart/helm"
   deployment_name        = "dashboard-${var.deployment_environment}"
   deployment_environment = "kube-system"
